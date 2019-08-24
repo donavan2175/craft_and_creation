70a91ee32074186d21464c39b1ba563989e5e387
Donavan Armstrong <donavan.mc@gmail.com>
2019-08-24 16:10:11 -0700

    u

---
+++ js/script.js
@@ -0,0 +1,365 @@
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
+var modal5 = document.getElementById("myModal5");
+var modal6 = document.getElementById("myModal6");
+var modal7 = document.getElementById("myModal7");
+var modal8 = document.getElementById("myModal8");
+var modal9 = document.getElementById("myModal9");
+var modal10 = document.getElementById("myModal10");
+var modal11 = document.getElementById("myModal11");
+var modal12 = document.getElementById("myModal12");
+var modal13 = document.getElementById("myModal13");
+var modal14 = document.getElementById("myModal14");
+var modal15 = document.getElementById("myModal15");
+var modal16 = document.getElementById("myModal16");
+var modal17 = document.getElementById("myModal17");
+var modal18 = document.getElementById("myModal18");
+var modal19 = document.getElementById("myModal19");
+var modal20 = document.getElementById("myModal20");
+
+
+var img = document.getElementById("image1");
+var img2 = document.getElementById("image2");
+var img3 = document.getElementById("image3");
+var img4 = document.getElementById("image4");
+var img5 = document.getElementById("image5");
+var img6 = document.getElementById("image6");
+var img7 = document.getElementById("image7");
+var img8 = document.getElementById("image8");
+var img9 = document.getElementById("image9");
+var img10 = document.getElementById("image10");
+var img11 = document.getElementById("image11");
+var img12 = document.getElementById("image12");
+var img13 = document.getElementById("image13");
+var img14 = document.getElementById("image14");
+var img15 = document.getElementById("image15");
+var img16 = document.getElementById("image16");
+var img17 = document.getElementById("image17");
+var img18 = document.getElementById("image18");
+var img19 = document.getElementById("image19");
+var img20 = document.getElementById("image20");
+
+var modalImg = document.getElementById("img");
+var modalImg2 = document.getElementById("img2");
+var modalImg3 = document.getElementById("img3");
+var modalImg4 = document.getElementById("img4");
+var modalImg5 = document.getElementById("img5");
+var modalImg6 = document.getElementById("img6");
+var modalImg7 = document.getElementById("img7");
+var modalImg8 = document.getElementById("img8");
+var modalImg9 = document.getElementById("img9");
+var modalImg10 = document.getElementById("img10");
+var modalImg11 = document.getElementById("img11");
+var modalImg12 = document.getElementById("img12");
+var modalImg13 = document.getElementById("img13");
+var modalImg14 = document.getElementById("img14");
+var modalImg15 = document.getElementById("img15");
+var modalImg16 = document.getElementById("img16");
+var modalImg17 = document.getElementById("img17");
+var modalImg18 = document.getElementById("img18");
+var modalImg19 = document.getElementById("img19");
+var modalImg20 = document.getElementById("img20");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+img2.onclick = function(){
+    modal2.style.display = "block";
+    modalImg2.src = this.src;
+  }
+
+img3.onclick = function(){
+  modal3.style.display = "block";
+  modalImg3.src = this.src;
+}
+
+img4.onclick = function(){
+  modal4.style.display = "block";
+  modalImg4.src = this.src;
+}
+
+img5.onclick = function(){
+  modal5.style.display = "block";
+  modalImg5.src = this.src;
+}
+
+img6.onclick = function(){
+  modal6.style.display = "block";
+  modalImg6.src = this.src;
+}
+
+img7.onclick = function(){
+    modal7.style.display = "block";
+    modalImg7.src = this.src;
+  }
+
+img8.onclick = function(){
+  modal8.style.display = "block";
+  modalImg8.src = this.src;
+}
+
+img9.onclick = function(){
+  modal9.style.display = "block";
+  modalImg9.src = this.src;
+}
+
+img10.onclick = function(){
+  modal10.style.display = "block";
+  modalImg10.src = this.src;
+}
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+img2.onclick = function(){
+    modal2.style.display = "block";
+    modalImg2.src = this.src;
+  }
+
+img3.onclick = function(){
+  modal3.style.display = "block";
+  modalImg3.src = this.src;
+}
+
+img4.onclick = function(){
+  modal4.style.display = "block";
+  modalImg4.src = this.src;
+}
+
+img5.onclick = function(){
+  modal5.style.display = "block";
+  modalImg5.src = this.src;
+}
+
+img6.onclick = function(){
+  modal6.style.display = "block";
+  modalImg6.src = this.src;
+}
+
+img7.onclick = function(){
+    modal7.style.display = "block";
+    modalImg7.src = this.src;
+  }
+
+img8.onclick = function(){
+  modal8.style.display = "block";
+  modalImg8.src = this.src;
+}
+
+img9.onclick = function(){
+  modal9.style.display = "block";
+  modalImg9.src = this.src;
+}
+
+img10.onclick = function(){
+  modal10.style.display = "block";
+  modalImg10.src = this.src;
+}
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+img2.onclick = function(){
+    modal2.style.display = "block";
+    modalImg2.src = this.src;
+  }
+
+img3.onclick = function(){
+  modal3.style.display = "block";
+  modalImg3.src = this.src;
+}
+
+img4.onclick = function(){
+  modal4.style.display = "block";
+  modalImg4.src = this.src;
+}
+
+img5.onclick = function(){
+  modal5.style.display = "block";
+  modalImg5.src = this.src;
+}
+
+img6.onclick = function(){
+  modal6.style.display = "block";
+  modalImg6.src = this.src;
+}
+
+img7.onclick = function(){
+    modal7.style.display = "block";
+    modalImg7.src = this.src;
+  }
+
+img8.onclick = function(){
+  modal8.style.display = "block";
+  modalImg8.src = this.src;
+}
+
+img9.onclick = function(){
+  modal9.style.display = "block";
+  modalImg9.src = this.src;
+}
+
+img10.onclick = function(){
+  modal10.style.display = "block";
+  modalImg10.src = this.src;
+}
+
+img11.onclick = function(){
+  modal11.style.display = "block";
+  modalImg11.src = this.src;
+}
+
+img12.onclick = function(){
+    modal12.style.display = "block";
+    modalImg12.src = this.src;
+  }
+
+img13.onclick = function(){
+  modal13.style.display = "block";
+  modalImg13.src = this.src;
+}
+
+img14.onclick = function(){
+  modal14.style.display = "block";
+  modalImg14.src = this.src;
+}
+
+img15.onclick = function(){
+  modal15.style.display = "block";
+  modalImg15.src = this.src;
+}
+
+img16.onclick = function(){
+  modal16.style.display = "block";
+  modalImg16.src = this.src;
+}
+
+img17.onclick = function(){
+    modal17.style.display = "block";
+    modalImg17.src = this.src;
+  }
+
+img18.onclick = function(){
+  modal18.style.display = "block";
+  modalImg18.src = this.src;
+}
+
+img19.onclick = function(){
+  modal19.style.display = "block";
+  modalImg19.src = this.src;
+}
+
+img20.onclick = function(){
+  modal20.style.display = "block";
+  modalImg20.src = this.src;
+}
+
+
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
+var span4 = document.getElementsByClassName("close")[3];
+var span5 = document.getElementsByClassName("close")[4];
+var span6 = document.getElementsByClassName("close")[5];
+var span7 = document.getElementsByClassName("close")[6];
+var span8 = document.getElementsByClassName("close")[7];
+var span9 = document.getElementsByClassName("close")[8];
+var span10 = document.getElementsByClassName("close")[9];
+var span11 = document.getElementsByClassName("close")[10];
+var span12 = document.getElementsByClassName("close")[11];
+var span13 = document.getElementsByClassName("close")[12];
+var span14 = document.getElementsByClassName("close")[13];
+var span15 = document.getElementsByClassName("close")[14];
+var span16 = document.getElementsByClassName("close")[15];
+var span17 = document.getElementsByClassName("close")[16];
+var span18 = document.getElementsByClassName("close")[17];
+var span19 = document.getElementsByClassName("close")[18];
+var span20 = document.getElementsByClassName("close")[19];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+span2.onclick = function() { 
+    modal2.style.display = "none";
+  }
+
+span3.onclick = function() { 
+  modal3.style.display = "none";
+}
+
+span4.onclick = function() { 
+  modal4.style.display = "none";
+}
+
+span5.onclick = function() { 
+  modal5.style.display = "none";
+}
+
+span6.onclick = function() { 
+  modal6.style.display = "none";
+}
+
+span7.onclick = function() { 
+  modal7.style.display = "none";
+}
+
+span8.onclick = function() { 
+  modal8.style.display = "none";
+}
+
+span9.onclick = function() { 
+  modal9.style.display = "none";
+}
+
+span10.onclick = function() { 
+  modal10.style.display = "none";
+}
+
+span11.onclick = function() { 
+  modal11.style.display = "none";
+}
+
+span12.onclick = function() { 
+    modal12.style.display = "none";
+  }
+
+span13.onclick = function() { 
+  modal13.style.display = "none";
+}
+
+span14.onclick = function() { 
+  modal14.style.display = "none";
+}
+
+span15.onclick = function() { 
+  modal15.style.display = "none";
+}
+
+span16.onclick = function() { 
+  modal16.style.display = "none";
+}
+
+span17.onclick = function() { 
+  modal17.style.display = "none";
+}
+
+span18.onclick = function() { 
+  modal18.style.display = "none";
+}
+
+span19.onclick = function() { 
+  modal19.style.display = "none";
+}
+
+span20.onclick = function() { 
+  modal20.style.display = "none";
+}
